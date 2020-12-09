ed13bfaa28e5963c3f03caeb9e5523b20e073a48
seolin <iseolin76@gmail.com>
2020-12-09 20:04:39 +0900

    Add '/' route

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ class App {
   }
 }
 const app = new App().application;
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', './public/views');
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("start");
+  res.render('index.html');
 })
 app.listen(4000, () => console.log(`Open Server http://127.0.0.1:4000`)); 
